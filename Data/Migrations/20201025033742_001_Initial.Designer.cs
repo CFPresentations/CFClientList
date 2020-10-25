015160847b8ca5bbb6e67404057f97936a92c3c5
Antonio Raynor <araynor@core-techs.net>
2020-10-24 23:46:32 -0400

    Added primary key ID field to Contact model. Code First Databse Created. Possible student commit

--- Data/Migrations/20201025033742_001_Initial.Designer.cs
+++ Data/Migrations/20201025033742_001_Initial.Designer.cs
@@ -4,14 +4,16 @@ using CFClientList.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CFClientList.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201025033742_001_Initial")]
+    partial class _001_Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -8,6 +8,7 @@ namespace CFClientList.Models
 {
     public class Contact
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
